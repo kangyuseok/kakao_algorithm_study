0003745ef34be2c48c32188f0e7a306636cf8172
yzooop <qkrduswn0404@gmail.com>
2024-06-30 21:44:56 +0900

    민겸수 / 40분

--- veronica/BOJ_21314.py
+++ veronica/BOJ_21314.py
@@ -1,22 +1,21 @@
 import sys
-input = sys.stdin.readline().strip()
 
-mk = list(input)
+mk = list(sys.stdin.readline().strip())
 
 m_cnt = 0
 max_result = ''
 min_result = ''
 
 for i in mk:
-    if i == "m":
+    if i == 'M':
         m_cnt += 1
-    elif i == "k":
+    elif i == 'K':
         if m_cnt > 0:
             max_result += '5' + '0' * m_cnt
             min_result += '1' + '0' * (m_cnt - 1) + '5'
         else:
-            max_result += 5
-            max_result += 5
+            max_result += '5'
+            min_result += '5'
         m_cnt = 0
 
 if m_cnt > 0:

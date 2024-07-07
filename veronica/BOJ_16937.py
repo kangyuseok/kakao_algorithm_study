d0c39f7ee82ab142a87a17f10c5bef12879f31cc
yzooop <qkrduswn0404@gmail.com>
2024-07-07 23:18:13 +0900

    ABCDE / 한시간

--- veronica/BOJ_16937.py
+++ veronica/BOJ_16937.py
@@ -2,17 +2,17 @@ import sys
 
 def dfs(node, depth):
     if depth >= 4:
-        return True
+        print(1)
+        sys.exit(0)  # 프로그램 종료
     
-    v[node] = 1 # 현재 노드 방문
+    v[node] = 1  # 현재 노드 방문
 
-    for next in adj[node]: # 인접 노드 순회
+    for next in adj[node]:  # 인접 노드 순회
         if v[next] == 0:  # 방문 안했으면
-            if dfs(next, depth + 1): # 방문하되, 깊이 들어가면 true
+            if dfs(next, depth + 1):  # 방문하고 깊이 증가
                 return True
             
     v[node] = 0 
-    
     return False
 
 # 입력 처리
@@ -26,7 +26,6 @@ for _ in range(M):
 v = [0] * N 
 
 for i in range(N):
-    v = [0] * N  # 방문 여부를 저장하는 배열
     if dfs(i, 0):
         print(1)
         sys.exit(0)  # 프로그램 종료
